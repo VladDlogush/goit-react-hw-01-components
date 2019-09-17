4ca08c1fc1f6fc2a88bbd8754cf32bd1bc952707
VladDlogush <vlad.dlogush21@gmail.com>
2019-09-17 21:00:24 +0300

    Updates

---
+++ precache-manifest.4d5f581d6025354ebcd0b35754dc407d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9aae07755635d1c0bc478e795024415",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "8e129aefc58bb4c3d6e0",
+    "url": "/goit-react-hw-01-components/static/css/main.78edd3e8.chunk.css"
+  },
+  {
+    "revision": "ce6f5926e976100c7318",
+    "url": "/goit-react-hw-01-components/static/js/2.38378226.chunk.js"
+  },
+  {
+    "revision": "8e129aefc58bb4c3d6e0",
+    "url": "/goit-react-hw-01-components/static/js/main.bb8ef9b1.chunk.js"
+  },
+  {
+    "revision": "bfb5c25008325e80740b",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.040dc6c8.js"
+  }
+]);
