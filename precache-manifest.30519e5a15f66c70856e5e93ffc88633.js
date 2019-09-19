ae8f396b53dcdafd08a9bd45c3d761f40a08af07
VladDlogush <vlad.dlogush21@gmail.com>
2019-09-19 16:10:25 +0300

    Updates

---
+++ precache-manifest.30519e5a15f66c70856e5e93ffc88633.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aabe19bf0f9be3af6cedd825acefc536",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "0085e0e44e66d49cd605",
+    "url": "/goit-react-hw-01-components/static/css/main.1e4982b6.chunk.css"
+  },
+  {
+    "revision": "39b347dfd7454eab1320",
+    "url": "/goit-react-hw-01-components/static/js/2.725e7f2b.chunk.js"
+  },
+  {
+    "revision": "0085e0e44e66d49cd605",
+    "url": "/goit-react-hw-01-components/static/js/main.c5f34e71.chunk.js"
+  },
+  {
+    "revision": "bfb5c25008325e80740b",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.040dc6c8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.4d5f581d6025354ebcd0b35754dc407d.js"
+  "/goit-react-hw-01-components/precache-manifest.30519e5a15f66c70856e5e93ffc88633.js"
 );
 
 self.addEventListener('message', (event) => {
